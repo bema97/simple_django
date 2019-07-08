4d9db49ce15b36c2407b104e632b65eb509408ec
bema97 <ibermet@gmail.com>
2019-07-08 13:42:39 +0600

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
-# Create your views here.
+from django.shortcuts import render 
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ['title', 'author', 'body']
+
+#
+# class BlogUpdateView(UpdateView):
+#     model = Post
+#     template_name = 'post_edit.html'
+#     fields = ['title', 'body']
+#
+#
+# class BlogDeleteView(DeleteView):
+#     model = Post
+#     template_name = 'post_delete.html'
+#     success_url = reverse_lazy('home')
